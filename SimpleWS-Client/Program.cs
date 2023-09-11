a11414c447ef97446b6c71fd29b66d39960fab02
İsmail KURT <ikurt@smartpulse.io>
2023-09-11 11:27:45 +0300

    Add project files.

---
+++ SimpleWS-Client/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+var ws = new ClientWebSocket();
+
+Console.WriteLine("Connecting to Server");
+await ws.ConnectAsync(new Uri("ws://localhost:6969/ws"), CancellationToken.None);
+Console.WriteLine("Connected");
+
+var reciveTask = Task.Run(async () =>
+{
+    var buffer = new byte[1024 * 4];
+    while (true)
+    {
+        var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        if (result.MessageType == WebSocketMessageType.Close)
+        {
+            break;
+        }
+
+        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+        Console.WriteLine(message);
+    }
+});
+await reciveTask;
+
+
+
+// ÇOKLU WEBSOCKET CLİENT
+
+/*
+string name;
+
+while (true)
+{
+    Console.Write("Input name: ");
+    name = Console.ReadLine();
+    break;
+}
+
+Console.WriteLine("Connecting to Server");
+await ws.ConnectAsync(new Uri($"ws://localhost:6969/ws?name={name}"), CancellationToken.None);
+Console.WriteLine("Connected");
+
+var reciveTask = Task.Run(async () =>
+{
+    var buffer = new byte[1024 * 4];
+     while (true)
+    {
+        var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        if (result.MessageType == WebSocketMessageType.Close)
+        {
+            break;
+        }
+
+        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+        Console.WriteLine(message);
+    }
+});
+
+var sendTask = Task.Run(async () =>
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+
+        if (message == "exit")
+        {
+            break;
+        }
+
+        var bytes = Encoding.UTF8.GetBytes(message);
+        await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+});
+
+await Task.WhenAll(sendTask, reciveTask);
+
+if (ws.State != WebSocketState.Closed)
+{
+    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+}
+
+await Task.WhenAll(sendTask, reciveTask);
+*/
---
+++ SimpleWS-Server/Program.cs
@@ -0,0 +1,152 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://localhost:6969");
+
+var app = builder.Build();
+app.UseWebSockets();
+
+var data = new Notificaitons
+           {
+                Id = "468d9d99-b195-4b6a-944e-a0b468747c91",
+                Status = 0,
+                Type = "excelimportcomplete",
+                WebUrl = "updatedata/ST/",
+                TargetEntityId = "",
+                Description = "•Poyraz RES santraline ait veri -> EAK (MW) - 2023090703 /  Güncel Veri : 25.0\n•Poyraz RES santraline ait veri -> EAK (MW) - 2023090702 /  Güncel Veri : 25.0\n•Poyraz RES santraline ait veri -> EAK (MW) - 2023090701 /  Güncel Veri : 25.0\n•Poyraz RES santraline ait veri -> EAK (MW) - 2023090700 /  Güncel Veri : 25.0\n"
+            };
+
+var responseNotification = new NotificaitonList
+                           {
+                               ErrorCode = 0,
+                               ResultMessage = "",
+                               Success = false,
+                               Data = data
+                            };
+
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+        while (true)
+        {
+            var message = "The current time is: " + DateTime.Now.ToString("HH:mm:ss");
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+
+            string jsonString = JsonSerializer.Serialize(responseNotification);
+            var bytes2 = Encoding.UTF8.GetBytes(jsonString);
+            var arraySegment2 = new ArraySegment<byte>(bytes2, 0, bytes2.Length);
+
+            if (ws.State == WebSocketState.Open)
+            {
+                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                await ws.SendAsync(arraySegment2, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+            {
+                break;
+            }
+            Thread.Sleep(5000);
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
+
+
+
+// ÇOKLU WEBSOCKET SERVER
+
+/*
+var connections = new List<WebSocket>();
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var curName = context.Request.Query["name"];
+
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+
+        connections.Add(ws);
+
+        await Broadcast($"{curName} joined the room");
+        await Broadcast($"{connections.Count} users connected");
+        await ReciveMessage(ws, async (result, buffer) =>
+        {
+            if (result.MessageType == WebSocketMessageType.Text)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                await Broadcast(curName + ": " + message);
+            }
+            else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+            {
+                connections.Remove(ws);
+                await Broadcast($"{curName} left the room");
+                await Broadcast($"{connections.Count} users connected");
+                await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+        });
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
+
+async Task ReciveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+{
+    var buffer = new byte[1024 * 4];
+    while (socket.State == WebSocketState.Open)
+    {
+        var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        handleMessage(result, buffer);
+    }
+}
+
+async Task Broadcast(string message)
+{
+    var bytes = Encoding.UTF8.GetBytes(message);
+    foreach (var socket in connections)
+    {
+        if (socket.State == WebSocketState.Open) 
+        {
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
+*/
+await app.RunAsync();
+
+public class NotificaitonList
+{
+    public int ErrorCode { get; set; }
+    public bool Success { get; set; }
+    public string ResultMessage { get; set; } = "";
+    public Notificaitons Data { get; set; }
+
+
+}
+public class Notificaitons
+{
+    public string Id { get; set; }
+    public int Status { get; set; }
+
+    public string Type { get; set; }
+    public string WebUrl { get; set; }
+    public string TargetEntityId { get; set; }
+    public string Description { get; set; }
+    public DateTime CreateDate { get; set; } = DateTime.Now;
+}
+
